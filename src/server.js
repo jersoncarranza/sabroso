8ad52b43e5aba9a613bf8d7cee43fb1aacae2407
unknown <jerson_carranza2@hotmail.com>
2020-06-30 23:56:15 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express'),
+      path    = require('path');
+const app = express();
+//var server_host = process.env.YOUR_HOST || '0.0.0.0';
+var PORT        =  process.env.PORT || 8000;
+
+app.use(express.static(__dirname + '/dist/FrontMatch'));
+
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname + '/dist/FrontMatch/index.html'));
+});
+
+
+app.listen(PORT);
