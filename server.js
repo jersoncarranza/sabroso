8c9534f523d58744a3dbfda9115b5371d562bc36
unknown <jerson_carranza2@hotmail.com>
2020-07-02 12:59:30 -0500

    super2002

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express'),
       path    = require('path');
 const app = express();
+const ngExpressEngine = require ('@nguniversal/express-engine');
 //var server_host = process.env.YOUR_HOST || '0.0.0.0';
 var PORT        =  process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/dist/FrontMatch'));
+app.engine('html', ngExpressEngine({
+    bootstrap: ServerAppModule // Give it a module to bootstrap
+  }));
 
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/FrontMatch/index.html'));
-  //  res.sendFile('index.html', {root: 'dist/FrontMatch/'});
+    //res.sendFile(path.join(__dirname + '/dist/FrontMatch/index.html'));
+    let url = 'http://someurl.com';
+    let doc = '<html><head><title>Seo en Angular 9</title></head><body><h1>Los pasos para indexar angular en GOOGLE (SEO)</h1> </body></html>';
+    res.render('../dist/index', {
+      req,
+      res,
+      url,
+      document: doc
+    });
 });
 
 
--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,9 +1,9 @@
 export var GLOBAL ={
     //mocoraxon
-    //url:'http://localhost:4000/',
-    //urlPublic:'http://localhost:4000/',
+    url:'http://localhost:4000/',
+    urlPublic:'http://localhost:4000/',
 
-   url:'https://app-meet.herokuapp.com/',  // Production
-    urlPublic:'https://app-meet.herokuapp.com/',// Production
+  // url:'https://app-meet.herokuapp.com/',  // Production
+   // urlPublic:'https://app-meet.herokuapp.com/',// Production
     urlcloudinary:'https://res.cloudinary.com/djempmk2c/image/upload/'
 }
